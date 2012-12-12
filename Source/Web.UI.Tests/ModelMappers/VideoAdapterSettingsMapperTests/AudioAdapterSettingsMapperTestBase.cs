bcf749732cd748ac5267bdeab3969b22d041f6fb
Erwin Konink <erwinkonink@gmail.com>
2012-12-12 10:55:39 +0100

    Added Video mappers

---
+++ Source/Web.UI.Tests/ModelMappers/VideoAdapterSettingsMapperTests/AudioAdapterSettingsMapperTestBase.cs
@@ -0,0 +1,16 @@
+﻿using Ewk.BandWebsite.Web.UI.ModelMappers;
+
+namespace Ewk.BandWebsite.Web.UI.Tests.ModelMappers.VideoAdapterSettingsMapperTests
+{
+    public abstract class VideoAdapterSettingsMapperTestBase : ModelMappersTestBase
+    {
+        protected VideoAdapterSettingsMapper Mapper { get; private set; }
+
+        protected override void AdditionalSetup()
+        {
+            base.AdditionalSetup();
+
+            Mapper = new VideoAdapterSettingsMapper(CatalogsContainer);
+        }
+    }
+}
